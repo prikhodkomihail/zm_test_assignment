16e9841a309f105973cc4f177fbaed5ad870d619
Prikhodko Mikhail <prih179@gmail.com>
2025-01-10 18:39:46 +0700

    Переименованы названия тестовых файлов

--- automated_testing/conftest.py
+++ automated_testing/conftest.py
@@ -8,8 +8,9 @@ def browser():
     options = Options()
     options.add_argument('--ignore-certificate-errors')
     options.add_argument('--ignore-ssl-errors')
+    options.add_argument('--disable-web-security')
     browser = webdriver.Chrome(options=options)
-    browser = webdriver.Chrome()
+
     browser.implicitly_wait(10)
     yield browser
     browser.quit()
